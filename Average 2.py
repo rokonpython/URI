765d3a93cb6a7872f6ff38c75f3069cec2ddf684
Rokon <84969208+rokonpython@users.noreply.github.com>
2021-05-28 17:29:54 +0600

    Create Average 2.py

---
+++ Average 2.py
@@ -0,0 +1,8 @@
+x=float(input())
+y=float(input())
+z=float(input())
+result_of_x=(x*2)
+result_of_y=(y*3)
+result_of_z=(z*5)
+result=(result_of_x+result_of_y+result_of_z)/10
+print('MEDIA = %.1f'%result)
